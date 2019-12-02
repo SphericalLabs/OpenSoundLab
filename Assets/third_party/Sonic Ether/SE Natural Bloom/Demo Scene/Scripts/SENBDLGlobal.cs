fa7c8bb6561ca13d8c45a1956bfadeee31143cd7
JWS <james.surine@gmail.com>
2019-12-01 19:01:53 -0600

    Remove excess SENaturalBloomAndDirtyLens stuff and only check in original files from open source version so Unity will properly find them and link them up to existing stuff.

--- Assets/third_party/Sonic Ether/SE Natural Bloom/Demo Scene/Scripts/SENBDLGlobal.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public static class SENBDLGlobal
-{
-	public static Quaternion sphereOfCubesRotation;
-	public static SENBDLMainCube mainCube;
-	public static float timeScale;
-}
