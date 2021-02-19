9bef993a55d0dcb346fec9df9691f737498c3783
Ludwig Zeller <ludwig.zeller@fhnw.ch>
2021-02-19 09:56:07 +0100

    added preloaded 360 environments

---
+++ Assets/Switch360.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch360 : MonoBehaviour
+{
+    public List<Texture> environments;
+    public float lastPointer = 0;
+    float newPointer = 0f;
+    Material mat;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().materials[0];
+        mat.mainTexture = environments[0];
+    }
+
+    void Update()
+    {
+
+        newPointer += Mathf.Pow(OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x, 2) * 0.2f;
+        if (newPointer > environments.Count - 1)
+        {
+            newPointer = 0;
+        }
+        else if (newPointer < 0)
+        {
+            newPointer = environments.Count - 1;
+        }
+        if (Mathf.RoundToInt(lastPointer) != Mathf.RoundToInt(newPointer)) {
+            mat.mainTexture = environments[Mathf.RoundToInt(newPointer)];
+        }
+        lastPointer = newPointer;
+
+    }
+}
