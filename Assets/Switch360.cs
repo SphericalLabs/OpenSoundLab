c9dd779789c4b79bb7fd9a8e03941236b69011e6
Ludwig Zeller <ludwig.zeller@fhnw.ch>
2021-02-20 16:09:10 +0100

    improved 360 pano view selection, added rotation

--- Assets/Switch360.cs
+++ Assets/Switch360.cs
@@ -7,30 +7,62 @@ public class Switch360 : MonoBehaviour
     public List<Texture> environments;
     public float lastPointer = 0;
     float newPointer = 0f;
+    int pointer = 0;
     Material mat;
+    Vector2 leftStick;
+    Vector3 rotation;
 
     void Start()
     {
         mat = GetComponent<Renderer>().materials[0];
         mat.mainTexture = environments[0];
+        rotation = new Vector3(0f, 0f, 0f);
     }
 
     void Update()
     {
+        leftStick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, OVRInput.Controller.LTouch);
 
-        newPointer += Mathf.Pow(OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x, 2) * 0.2f;
-        if (newPointer > environments.Count - 1)
+        // rotate 360 sphere around y-axis
+        if (leftStick.x != 0f)
         {
-            newPointer = 0;
+            rotation.y = -1 * Mathf.Sign(leftStick.x) * Mathf.Abs(Mathf.Pow(leftStick.x, 4) * 1f);
+            transform.Rotate(rotation);
         }
-        else if (newPointer < 0)
-        {
-            newPointer = environments.Count - 1;
-        }
-        if (Mathf.RoundToInt(lastPointer) != Mathf.RoundToInt(newPointer)) {
-            mat.mainTexture = environments[Mathf.RoundToInt(newPointer)];
-        }
-        lastPointer = newPointer;
 
+        // shuttle through 360 panos, allows for hi-speed fun...
+        //if (leftStick.y != 0f) { 
+        //newPointer += Mathf.Sign(leftStick.y) * Mathf.Abs(Mathf.Pow(leftStick.y, 3) * 0.3f);
+        //if (newPointer > environments.Count - 1)
+        //{
+        //  newPointer = 0;
+        //}
+        //else if (newPointer < 0)
+        //{
+        //  newPointer = environments.Count - 1;
+        //}
+        //if (Mathf.RoundToInt(lastPointer) != Mathf.RoundToInt(newPointer)) {
+        //  mat.mainTexture = environments[Mathf.RoundToInt(newPointer)];
+        //}
+        //lastPointer = newPointer;
+
+        //}
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryThumbstickUp, OVRInput.Controller.LTouch))
+        {
+            pointer = mod(pointer + 1, environments.Count);
+            mat.mainTexture = environments[pointer];
+        }
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryThumbstickDown, OVRInput.Controller.LTouch))
+        {
+            pointer = mod(pointer - 1, environments.Count);
+            mat.mainTexture = environments[pointer];
+        }
+
+    }
+    int mod(int a, int b)
+    {
+        int c = a % b;
+        return (c < 0) ? c + b : c;
     }
 }
