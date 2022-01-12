923b642c006267a42d0d0ca5f8794ba5cc554abd
Ludwig Zeller <ludwig.zeller@fhnw.ch>
2022-01-12 17:18:01 +0100

    adjusted build script

--- Assets/SoundStageNative/SoundStageNative/build_plugin.sh
+++ Assets/SoundStageNative/SoundStageNative/build_plugin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ $USER == "hannes.barfuss@fhnw.ch" ]
+if [[ $USER == "hannes.barfuss@fhnw.ch" ]]
 then
 	NDK_PATH="/Users/hannes.barfuss@fhnw.ch/Library/Android/sdk/ndk/23.1.7779620/ndk-build"
 	PLUGIN_PATH="/Users/hannes.barfuss@fhnw.ch/Documents/dev/soundstage/soundstagevr-quest/Assets/SoundStageNative/SoundStageNative"
@@ -15,7 +15,7 @@ else
 	echo ""
 	echo "Compiling native code..."
 
-	arch $NDK_PATH NDK_DEBUG=1 NDK_PROJECT_PATH=. NDK_APPLICATION_MK=./Application.mk $*
+	$NDK_PATH NDK_DEBUG=1 NDK_PROJECT_PATH=. NDK_APPLICATION_MK=./Application.mk $*
 	mv libs/armeabi-v7a/libSoundStageNative.so ./armv7/Release/libSoundStageNative.so
 fi
 
