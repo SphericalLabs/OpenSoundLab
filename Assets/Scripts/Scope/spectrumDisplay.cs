4da28e072e2a769a3c810cf99a66addd85f19f66
Ludwig Zeller <ludwig.zeller@fhnw.ch>
2022-01-17 17:56:49 +0100

    Scope: Switch over to OpenGL for drawing the spectrum; Should be faster, but might need further optimisation and testing. Bug: first band of FFT not drawn at the moment.

--- Assets/Scripts/Scope/spectrumDisplay.cs
+++ Assets/Scripts/Scope/spectrumDisplay.cs
@@ -17,133 +17,121 @@ using System.Collections;
 using System.Diagnostics;
 using System;
 
-public class spectrumDisplay : MonoBehaviour {
+public class spectrumDisplay : MonoBehaviour
+{
   public AudioSource source;
   public FFTWindow fftWin = FFTWindow.BlackmanHarris;
   float[] spectrum;
 
-  int texW = 256*4;
-  int texH = 256*1;
-  Texture2D tex;
-  public Renderer texrend;
-  Color32[] texpixels;
-  Color32[] blackpixels;
-  public Color32 onColor = new Color32(255, 255, 255, 255); 
-  public Color32 offColor = new Color32(0, 0, 0, 255);
-    
-  public bool doSpectrum = true;
-  public bool doClear1 = true;
-  public bool doDraw = true;
+  int width = 256 * 4;
+  int height = 256 * 1;
+
+  RenderTexture offlineTexture;
+  Material offlineMaterial;
+
+  Texture2D onlineTexture;
+  public Material onlineMaterial;
+
   bool active = false;
 
   int drawY = 0;
-  int drawX = 0;  
+  int drawX = 0;
   int lastDrawX = 0;
   int lastDrawY = 0;
   public int skip = 1;
   float bandWidth;
   float maxLog;
 
-  void Start() {
-    spectrum = new float[texW];
+  void Start()
+  {
+    spectrum = new float[width];
 
-    tex = new Texture2D(texW, texH, TextureFormat.RGBA32, false);
-    texpixels = new Color32[texW * texH];
-    blackpixels = new Color32[texW * texH];
-
-    for (int i = 0; i < blackpixels.Length; i++) blackpixels[i] = offColor;
-    tex.SetPixels32(blackpixels);
-    tex.Apply(true);
-
-    texrend.material.mainTexture = tex;
-    texrend.material.SetTexture(Shader.PropertyToID("_MainTex"), tex);
-
-    bandWidth = 24000f / texW;
+    bandWidth = 24000f / width;
     maxLog = Mathf.Log10(24000);
 
+    // create material for GL rendering, is this like an actual drawing material? 
+    // But colors, etc. can be set below, too... 
+    // code doesn't work without this though!
+    offlineMaterial = new Material(Shader.Find("GUI/Text Shader"));
+    offlineMaterial.hideFlags = HideFlags.HideAndDontSave;
+    offlineMaterial.shader.hideFlags = HideFlags.HideAndDontSave;
+    // get a RenderTexture and set it as target for offline rendering
+    offlineTexture = RenderTexture.GetTemporary(width, height);
+    RenderTexture.active = offlineTexture;
+
+    // these are the ones that you will actually see
+    onlineTexture = new Texture2D(width, height, TextureFormat.ARGB32, false);
+    onlineMaterial = Instantiate(onlineMaterial);
+    GetComponent<Renderer>().material = onlineMaterial;
+
   }
 
-  
-
-  void GenerateTex() {
-
-    // Please note: Ideally this would be ported to native code and OpenGL?
-    // Fallback, decrease fps: 72, 36, 18
-    // run multi-threaded? https://www.raywenderlich.com/7880445-unity-job-system-and-burst-compiler-getting-started
-
-    if (doClear1)
-    {
-      Array.Copy(blackpixels, texpixels, blackpixels.Length);
-    }
-        
-    if (doDraw)
-    {
-      // reset for this frame
-      drawY = 0;
-      drawX = 0;
-      lastDrawX = 0;
-      lastDrawY = 0;
-
-      for (int freqBand = 1; freqBand < spectrum.Length; freqBand++) // skip 0, because of log negative?
-      {
-        drawX = Mathf.RoundToInt(Utils.map(Mathf.Log10(freqBand * bandWidth), 1f, maxLog, 0f, texW));
-        if (drawX - lastDrawX <= skip) continue; // skip bands if too close together
-
-        drawY = Mathf.RoundToInt(Mathf.Pow(spectrum[freqBand], 0.3f) * (texH-1));
-
-        drawLine(texpixels, lastDrawX, lastDrawY, drawX, drawY, onColor);
-
-        //p1 = p2;
-        lastDrawX = drawX;
-        lastDrawY = drawY;
-      }
-
-      tex.SetPixels32(texpixels);
-      tex.Apply(); // apply takes time, from RAM to vRAM.
-    }
-  }
-
-  // Bresenham's line algorithm, ported from http://rosettacode.org/wiki/Bitmap/Bresenham%27s_line_algorithm#C.23
-  void drawLine(Color32[] pixels, int x0, int y0, int x1, int y1, Color color)
+  void Update()
   {
-    int dx = Math.Abs(x1 - x0), sx = x0 < x1 ? 1 : -1;  // move variables globally for speed
-    int dy = Math.Abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
-    int err = (dx > dy ? dx : -dy) / 2, e2;
-    for (; ; )
-    {
-      pixels[y0 * texW + x0] = color;
-      if (x0 == x1 && y0 == y1)
-      {
-        break;
-      }
-      e2 = err;
-      if (e2 > -dx) { err -= dy; x0 += sx; }
-      if (e2 < dy) { err += dx; y0 += sy; }
-    }
-  }
 
-  public void toggleActive(bool on) {
-    active = on;
-    if (!active) {
-      clearScreen();
-      tex.SetPixels32(texpixels);
-      tex.Apply(true);
-    }
-  }
-
-  void clearScreen(){
-    Array.Copy(blackpixels, texpixels, blackpixels.Length);
-  }
-
-  void Update() {
-    //Stopwatch timer = new Stopwatch();
-    //timer.Start();
     if (!active) return;
 
-    if(doSpectrum) source.GetSpectrumData(spectrum, 0, fftWin);
+    source.GetSpectrumData(spectrum, 0, fftWin);
+    RenderGLToTexture(width, height, offlineMaterial);
 
-    GenerateTex();
-    //timer.Stop();
-    //UnityEngine.Debug.Log(timer.ElapsedTicks);
   }
+
+  // Via https://forum.unity.com/threads/rendering-gl-to-a-texture2d-immediately-in-unity4.158918/
+  void RenderGLToTexture(int width, int height, Material material)
+  {
+
+    //Destroy(offlineTexture);
+
+    // get a temporary RenderTexture //
+    //offlineTexture = RenderTexture.GetTemporary(width, height);
+
+    // always reset offline render here, in case another script had its finger on it in the meantime
+    RenderTexture.active = offlineTexture;
+
+    GL.Clear(false, true, Color.black);
+
+    material.SetPass(0);
+    GL.PushMatrix();
+    GL.LoadPixelMatrix(0, width, height, 0);
+    GL.Begin(GL.LINE_STRIP);
+    GL.Color(new Color(1, 1, 1, 1f));
+
+    drawY = 0;
+    drawX = 0;
+    lastDrawX = 0;
+    lastDrawY = 0;
+
+    for (int freqBand = 0; freqBand < spectrum.Length; freqBand++) // skip 0, because of log negative?
+    {
+      drawX = Mathf.RoundToInt(Utils.map(Mathf.Log10(freqBand * bandWidth), 1f, maxLog, 0f, width));
+      if (drawX - lastDrawX <= skip) continue; // skip bands if too close together
+
+      drawY = height - Mathf.RoundToInt(Mathf.Pow(spectrum[freqBand], 0.3f) * height);
+
+      GL.Vertex3(drawX, drawY, 0);
+
+      //p1 = p2;
+      lastDrawX = drawX;
+      lastDrawY = drawY;
+    }
+
+    GL.End();
+    GL.PopMatrix();
+
+    // blit/copy the offlineTexture into the onlineTexture (on GPU)
+    Graphics.CopyTexture(offlineTexture, onlineTexture);
+
+    // See https://docs.unity3d.com/ScriptReference/RenderTexture.GetTemporary.html
+    //RenderTexture.active = null;
+    //RenderTexture.ReleaseTemporary(offlineTexture); 
+  }
+
+
+  public void toggleActive(bool on)
+  {
+    if (active == on) return;
+    active = on;
+    onlineMaterial.SetTexture(Shader.PropertyToID("_MainTex"), on ? onlineTexture : Texture2D.blackTexture);
+  }
+
 }
