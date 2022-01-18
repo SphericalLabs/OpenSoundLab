3a27f45ca18e7f30b7bc896a019dfc86c6a021e1
Hannes Barfuss <hannes.barfuss@fhnw.ch>
2022-01-18 18:19:46 +0100

    Updated paths in build_plugin.sh

--- SoundStageNative/build_plugin.sh
+++ SoundStageNative/build_plugin.sh
@@ -4,19 +4,22 @@ if [[ $USER == "hannes.barfuss@fhnw.ch" ]]
 then
 	NDK_PATH="/Users/hannes.barfuss@fhnw.ch/Library/Android/sdk/ndk/23.1.7779620/ndk-build"
 	PLUGIN_PATH="/Users/hannes.barfuss@fhnw.ch/Documents/dev/soundstage/soundstagevr-quest/Assets/SoundStageNative/SoundStageNative"
-	cd $PLUGIN_PATH
+	SOURCE_PATH="/Users/hannes.barfuss@fhnw.ch/Documents/dev/soundstage/soundstagevr-quest/SoundStageNative"
+	cd $SOURCE_PATH
 	echo ""
 	echo "Compiling native code..."
 
 	arch -x86_64 $NDK_PATH NDK_DEBUG=1 NDK_PROJECT_PATH=. NDK_APPLICATION_MK=./Application.mk $*
-	mv libs/armeabi-v7a/libSoundStageNative.so ./armv7/Release/libSoundStageNative.so
+
+	mv libs/arm64-v8a/libSoundStageNative.so ../Assets/SoundStageNative/x64/Release/libSoundStageNative.so
 else
+
 	NDK_PATH="/c/Users/Ludwig/android-ndk-r21e-windows-x86_64/android-ndk-r21e/build/ndk-build.cmd"
 	echo ""
 	echo "Compiling native code..."
 
 	$NDK_PATH NDK_PROJECT_PATH=. NDK_APPLICATION_MK=./Application.mk $*
-	mv libs/arm64-v8a/libSoundStageNative.so ./arm64/Release/libSoundStageNative.so
+	mv libs/arm64-v8a/libSoundStageNative.so ../Assets/SoundStageNative/x64/Release/libSoundStageNative.so
 fi
 
 echo ""
