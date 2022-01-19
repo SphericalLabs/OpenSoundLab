faa169f5900d78d18e9d728e2a3169b1259cf679
Hannes Barfuss <hannes.barfuss@fhnw.ch>
2022-01-19 18:55:23 +0100

    Forgot header in last commit

---
+++ SoundStageNative/CompressedRingBuffer.h
@@ -0,0 +1,27 @@
+//
+//  CompressedRingBuffer.hpp
+//  SoundStageNative
+//
+//  Created by hb on 19.01.22.
+//
+
+#ifndef CompressedRingBuffer_hpp
+#define CompressedRingBuffer_hpp
+
+#include "main.h"
+
+typedef struct _CompressedRingBuffer
+{
+    float* data;
+    float* weights;
+    int ptr;
+    int n;
+} CompressedRingBuffer;
+
+void CompressedRingBuffer_Read(float *dest, int n, int offset, float stride, CompressedRingBuffer *x);
+void CompressedRingBuffer_Write(float *src, int n, float stride, CompressedRingBuffer *x);
+CompressedRingBuffer *CompressedRingBuffer_New(int n);
+void CompressedRingBuffer_Free(CompressedRingBuffer *x);
+void CompressedRingBuffer_Print(CompressedRingBuffer *x);
+
+#endif /* CompressedRingBuffer_hpp */
