88c6dc69fb6bbe6e5046a204338b5c4b14cb3368
Ludwig Zeller <ludwig.zeller@fhnw.ch>
2022-03-16 20:38:56 +0100

    WorldDrag: added rotation

--- Assets/WorldDragController.cs
+++ Assets/WorldDragController.cs
@@ -10,7 +10,6 @@ public class WorldDragController : MonoBehaviour
   Vector3 startPosSpace, startPosLeft, startPosRight, midOfControllers;
   float startAngle;
   float newAngle;
-  Quaternion startQuat;
   bool isDragging = false;
 
   void Awake()
@@ -24,8 +23,10 @@ public class WorldDragController : MonoBehaviour
     if (leftManip == null) leftManip = GameObject.Find("ControllerLeft").GetComponentInChildren<manipulator>();
     if (rightManip == null) rightManip = GameObject.Find("ControllerRight").GetComponentInChildren<manipulator>();
 
-    if (!isDragging && (leftManip.isGrabbing() || rightManip.isGrabbing())) return;
+    // don't start the moving if grabbing something else already
+    if (!isDragging && (leftManip.isGrabbing() || rightManip.isGrabbing())) return; 
 
+    // begin drag, save start values
     if (!isDragging && OVRInput.Get(OVRInput.RawAxis1D.LHandTrigger) > 0.5f && OVRInput.Get(OVRInput.RawAxis1D.RHandTrigger) > 0.5f)
     {
       isDragging = true;
@@ -35,33 +36,29 @@ public class WorldDragController : MonoBehaviour
       startAngle = calcAngleBetweenControllers();
     }
     
+    // end of drag
     if (isDragging && (OVRInput.Get(OVRInput.RawAxis1D.LHandTrigger) < 0.3f || OVRInput.Get(OVRInput.RawAxis1D.RHandTrigger) < 0.3f) )
     {
       isDragging = false;
     }
 
+    // do the drag
     if(isDragging){
-      //midOfControllers = Vector3.Lerp(leftHandAnchor.localPosition, rightHandAnchor.localPosition, 0.5f);
-      //newAngle = calcAngleBetweenControllers();
 
-      //transform.Rotate(Vector3.up, newAngle, Space.Self);
-
-      
-      //transform.localPosition = midOfControllers;
-      //transform.RotateAround(midOfControllers, Vector3.up, transform.eulerAngles.y - newAngle);
-      //transform.localEulerAngles = new Vector3(0f, newAngle - startAngle, 0f);
-      
-      //Quaternion temp = transform.rotation;
-      //temp.eulerAngles = new Vector3(0f, newAngle - startAngle, 0f);
-      //transform.rotation = temp;
-
+      // translation
       transform.localPosition = startPosSpace + Vector3.Lerp(leftHandAnchor.localPosition - startPosLeft, rightHandAnchor.localPosition - startPosRight, 0.5f);
+
+      // rotation
+      midOfControllers = Vector3.Lerp(leftHandAnchor.localPosition, rightHandAnchor.localPosition, 0.5f);
+      newAngle = calcAngleBetweenControllers();
+      transform.rotation = Quaternion.identity; // first reset and then rotate, because RotateAround is adding up
+      transform.RotateAround(midOfControllers, Vector3.up, startAngle - newAngle);
       
-      //(midOfControllers, Vector3.up, Vector3.Angle(leftHandAnchor.localPosition, rightHandAnchor.localPosition) - startAngle);
     }
   }
 
   float calcAngleBetweenControllers(){
-    return Mathf.Rad2Deg * Mathf.Atan2(leftHandAnchor.transform.localPosition.y - rightHandAnchor.transform.localPosition.y, leftHandAnchor.transform.localPosition.x - rightHandAnchor.transform.localPosition.x);
+    // on x,z plane
+    return Mathf.Rad2Deg * Mathf.Atan2(leftHandAnchor.transform.localPosition.z - rightHandAnchor.transform.localPosition.z, leftHandAnchor.transform.localPosition.x - rightHandAnchor.transform.localPosition.x);
   }
 }
