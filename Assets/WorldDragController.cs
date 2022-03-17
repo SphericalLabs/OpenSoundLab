809e0b796b609ce496bf70b5f9e646fe3296e2c0
Ludwig Zeller <ludwig.zeller@fhnw.ch>
2022-03-17 15:39:38 +0100

    WorldDrag: rotation fully working as intended now

--- Assets/WorldDragController.cs
+++ Assets/WorldDragController.cs
@@ -7,14 +7,13 @@ public class WorldDragController : MonoBehaviour
   public manipulator leftManip, rightManip;
   public Transform leftHandAnchor, rightHandAnchor;
 
-  Vector3 patchPositionAtBeginDrag, controllerMiddleAtBeginDrag;
-  float controllerAngleAtBeginDrag, patchAngleAtBeginDrag;
-  float currentControllerAngle;
+  Vector3 lastControllerMiddle;
+  float currentControllerAngle, lastControllerAngle;
   bool isDragging = false;
 
   void Awake()
   {
-   
+    
   }
 
   void Update()
@@ -30,11 +29,9 @@ public class WorldDragController : MonoBehaviour
     if (!isDragging && OVRInput.Get(OVRInput.RawAxis1D.LHandTrigger) > 0.5f && OVRInput.Get(OVRInput.RawAxis1D.RHandTrigger) > 0.5f)
     {
       isDragging = true;
-      patchPositionAtBeginDrag = transform.position;
-      patchAngleAtBeginDrag = transform.rotation.eulerAngles.y;
 
-      controllerMiddleAtBeginDrag = getMiddle(leftHandAnchor, rightHandAnchor);
-      controllerAngleAtBeginDrag = getAngleBetweenControllers();
+      lastControllerMiddle = getMiddle(leftHandAnchor, rightHandAnchor);
+      lastControllerAngle = getAngleBetweenControllers();
     }
     
     // end of drag
@@ -46,37 +43,30 @@ public class WorldDragController : MonoBehaviour
     // do the drag
     if(isDragging){
 
-      Debug.DrawLine(Vector3.zero, patchPositionAtBeginDrag, new Color(1f, 0f, 0f));
-      Debug.DrawLine(Vector3.zero, new Vector3(Mathf.Sin(Mathf.Deg2Rad * patchAngleAtBeginDrag), 0f, Mathf.Cos(Mathf.Deg2Rad * patchAngleAtBeginDrag)), new Color(0f, 1f, 0f));
-      Debug.DrawLine(Vector3.zero, controllerMiddleAtBeginDrag, new Color(0f, 0f, 1f));
-      Debug.DrawLine(Vector3.zero, new Vector3(Mathf.Sin(Mathf.Deg2Rad * controllerAngleAtBeginDrag), 0f, Mathf.Cos(Mathf.Deg2Rad * controllerAngleAtBeginDrag)), new Color(1f, 1f, 0f));
-      Debug.DrawLine(Vector3.zero, new Vector3(Mathf.Sin(Mathf.Deg2Rad * currentControllerAngle), 0f, Mathf.Cos(Mathf.Deg2Rad * currentControllerAngle)), new Color(1f, 0f, 1f));
-      Debug.DrawLine(Vector3.zero, getMiddle(leftHandAnchor, rightHandAnchor), new Color(0f, 1f, 1f));
-
-      // translation
-      //transform.position = patchPositionAtBeginDrag + getMiddle(leftHandAnchor, rightHandAnchor) - controllerMiddleAtBeginDrag;
+      //Debug.DrawLine(Vector3.zero, patchPositionAtBeginDrag, new Color(1f, 0f, 0f));
+      //Debug.DrawLine(Vector3.zero, new Vector3(Mathf.Sin(Mathf.Deg2Rad * patchAngleAtBeginDrag), 0f, Mathf.Cos(Mathf.Deg2Rad * patchAngleAtBeginDrag)), new Color(0f, 1f, 0f));
+      //Debug.DrawLine(Vector3.zero, controllerMiddleAtBeginDrag, new Color(0f, 0f, 1f));
+      //Debug.DrawLine(Vector3.zero, new Vector3(Mathf.Sin(Mathf.Deg2Rad * controllerAngleAtBeginDrag), 0f, Mathf.Cos(Mathf.Deg2Rad * controllerAngleAtBeginDrag)), new Color(1f, 1f, 0f));
+      //Debug.DrawLine(Vector3.zero, new Vector3(Mathf.Sin(Mathf.Deg2Rad * currentControllerAngle), 0f, Mathf.Cos(Mathf.Deg2Rad * currentControllerAngle)), new Color(1f, 0f, 1f));
+      //Debug.DrawLine(Vector3.zero, getMiddle(leftHandAnchor, rightHandAnchor), new Color(0f, 1f, 1f));
 
       // rotation
       currentControllerAngle = getAngleBetweenControllers();
-      transform.rotation = Quaternion.identity; // first reset and then rotate, because RotateAround is adding up
+      transform.RotateAround(getMiddle(leftHandAnchor, rightHandAnchor), Vector3.up, lastControllerAngle - currentControllerAngle);
 
-      transform.RotateAround(getMiddle(leftHandAnchor, rightHandAnchor), Vector3.up, patchAngleAtBeginDrag + controllerAngleAtBeginDrag - currentControllerAngle);
-      //transform.RotateAround(transform.position, Vector3.up, patchAngleAtBeginDrag + controllerAngleAtBeginDrag - currentControllerAngle);
-      //transform.RotateAround(getPatchCenter(), Vector3.up, patchAngleAtBeginDrag + controllerAngleAtBeginDrag - currentControllerAngle);
-
-      transform.position = patchPositionAtBeginDrag + getMiddle(leftHandAnchor, rightHandAnchor) - controllerMiddleAtBeginDrag;
-
+      // translation
+      transform.Translate(getMiddle(leftHandAnchor, rightHandAnchor) - lastControllerMiddle, Space.World);
+      lastControllerMiddle = getMiddle(leftHandAnchor, rightHandAnchor);
+      lastControllerAngle = currentControllerAngle;
     }
   }
 
   Vector3 getPatchCenter(){
     Vector3 sumVector = new Vector3(0f, 0f, 0f);
-
     foreach (Transform child in transform)
     {
       sumVector += child.position;
     }
-
     return sumVector / transform.childCount;
   }
 
