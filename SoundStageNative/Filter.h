30fce3e2f3d09827d781ec96473dda14901ab171
Ludwig Zeller <ludwig.zeller@fhnw.ch>
2022-04-08 17:03:39 +0200

    license notes

--- SoundStageNative/Filter.h
+++ SoundStageNative/Filter.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Google LLC
+ * Copyright 2017-2022 Logan Olson, Google LLC, James Surine, Ludwig Zeller, Hannes Barfuss
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
