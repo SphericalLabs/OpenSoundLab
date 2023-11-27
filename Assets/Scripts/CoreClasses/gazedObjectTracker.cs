1cbc6e4cc76481712d75c8e2b4d54a16e9eb175a
Ludwig Zeller <contact@ludwigzeller.net>
2023-11-27 20:13:14 +0100

    Added a simple eye tracking calibration tool

--- Assets/Scripts/CoreClasses/gazedObjectTracker.cs
+++ Assets/Scripts/CoreClasses/gazedObjectTracker.cs
@@ -7,6 +7,9 @@ public class gazedObjectTracker : MonoBehaviour
     private static gazedObjectTracker instance;
     public manipObject gazedAtManipObject;
     public Vector3 correction;
+    public GameObject gazeIndicator;
+    public GameObject calibrationPlane;
+    public GameObject calibrationPlaneCenter;
 
     void Awake()
     {
@@ -39,28 +42,56 @@ public class gazedObjectTracker : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        // reset every frame
-        gazedAtManipObject = null;
+  // Update is called once per frame
+  void Update()
+  {
+    // reset every frame
+    gazedAtManipObject = null;
+    gazeIndicator.SetActive(false);
 
-        transform.Translate(correction); // shift 2cm 
+    Ray ray = new Ray(transform.position, transform.forward);
+    RaycastHit hit;
+    int layerMask = 1 << 9; // layerMask 9 = manipOnly
 
-        Ray ray = new Ray(transform.position, transform.forward);
-        RaycastHit hit;
-        int layerMask = 1 << 9; // layerMask 9 = manipOnly
+    if (Physics.Raycast(ray, out hit, 2f, layerMask)){ 
 
-        if (Physics.Raycast(ray, out hit, 2f, layerMask)) 
+      // if looking at calib plane and one trigger is pressed
+      if (hit.collider.gameObject == calibrationPlane)
+      {
+        
+        gazeIndicator.transform.position = hit.point;
+        gazeIndicator.SetActive(true);
+
+        if (Time.frameCount % 30 == 0 && (Input.GetAxis("triggerL") > 0.7 || Input.GetAxis("triggerR") > 0.7))
         {
-          manipObject targetObject = hit.collider.GetComponent<manipObject>();
-          if (targetObject != null)
-          {            
-            gazedAtManipObject = targetObject;
-            //targetObject.setState(manipObject.manipState.selected);
-            //Debug.Log("gaze set selected");
-          }
+          Vector3 localizedHitPoint = transform.InverseTransformPoint(hit.point);
+          Vector3 localizedPlaneCenter = transform.InverseTransformPoint(calibrationPlaneCenter.transform.position);
+          correction.x = localizedPlaneCenter.x - localizedHitPoint.x;
+          correction.y = localizedPlaneCenter.y - localizedHitPoint.y;
         }
+
+        return;
+      }
     }
+
+    transform.Translate(correction); 
+
+    ray = new Ray(transform.position, transform.forward);
+    layerMask = 1 << 9; // layerMask 9 = manipOnly
+
+    gazeIndicator.SetActive(false);
+
+    if (Physics.Raycast(ray, out hit, 2f, layerMask))
+    {
+      manipObject targetObject = hit.collider.GetComponent<manipObject>();
+      if (targetObject != null)
+      {
+        gazedAtManipObject = targetObject;
+        gazeIndicator.transform.position = hit.point;
+        gazeIndicator.SetActive(true);
+      }
+
+    }
+  }
 }
 
