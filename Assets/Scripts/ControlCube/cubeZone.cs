c99e2bd664750ab44c30645fb950e6aca0433e16
ChrisEL20 <mhptcg@gmail.com>
2024-04-16 11:34:56 +0200

    control cube spawn bugfixed

--- Assets/Scripts/ControlCube/cubeZone.cs
+++ Assets/Scripts/ControlCube/cubeZone.cs
@@ -36,80 +36,91 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class cubeZone : manipObject {
-  ControlCubeDeviceInterface _deviceInterface;
-  public Transform[] lines;
-  Material mat;
+public class cubeZone : manipObject
+{
+    ControlCubeDeviceInterface _deviceInterface;
+    public Transform[] lines;
+    Material mat;
 
-  Color onColor = new Color(0.414f, 0.444f, 0.750f, 0.466f);
-  Color offColor = new Color(0, 0, 0, .466f);
+    Color onColor = new Color(0.414f, 0.444f, 0.750f, 0.466f);
+    Color offColor = new Color(0, 0, 0, .466f);
 
-  public override void Awake() {
-    base.Awake();
-    mat = GetComponent<Renderer>().material;
-    _deviceInterface = GetComponentInParent<ControlCubeDeviceInterface>();
-    updatePercent(p);
+    public override void Awake()
+    {
+        base.Awake();
+        mat = GetComponent<Renderer>().material;
+        _deviceInterface = GetComponentInParent<ControlCubeDeviceInterface>();
+        updatePercent(p, false);
 
-    if (lines[0] != null) lineWidth = lines[0].localScale.x;
-  }
-
-  Vector3 controllerPosAtBeginDragging;
-  Transform tip;
-  public override void setGrab(bool on, Transform t){
-    base.setGrab(on, t);
-    tip = t.Find("manipCollViz").transform;
-    controllerPosAtBeginDragging = tip.position;
-    pAtBeginDragging = p;
-  }
-
-  Vector3 p, pAtBeginDragging = Vector3.zero;
-  float lineWidth = 0f;
-
-  public override void grabUpdate(Transform t) {
-    p = pAtBeginDragging + transform.InverseTransformPoint(tip.position) - transform.InverseTransformPoint(controllerPosAtBeginDragging);
-    p.x = Mathf.Clamp(p.x, -0.5f, 0.5f);
-    p.y = Mathf.Clamp(p.y, -0.5f, 0.5f);
-    p.z = Mathf.Clamp(p.z, -0.5f, 0.5f);
-    updatePercent(p);
-
-    if (manipulatorObjScript != null) manipulatorObjScript.hapticPulse((ushort)(700f * (_deviceInterface.percent.x + _deviceInterface.percent.y + _deviceInterface.percent.z) / 3 + 50f));
-  }
-
-  void updatePercent(Vector3 tmp) {
-    tmp.x = Mathf.Clamp01(-tmp.x + .5f);
-    tmp.y = Mathf.Clamp01(tmp.y + .5f);
-    tmp.z = Mathf.Clamp01(-tmp.z + .5f);
-
-    _deviceInterface.updatePercent(tmp, true);
-    updateLines(tmp);
-  }
-
-  float lineMargin = 0.0015f;
-  public void updateLines(Vector3 p) {
-    p = p * (.3f - lineMargin * 2) + Vector3.one * lineMargin; // margin to align with inside borders
-    lines[0].localPosition = new Vector3(-p.x + .15f,   p.y,   0);
-    lines[1].localPosition = new Vector3(-p.x + .15f,   .15f,  -p.z + .15f);
-    lines[2].localPosition = new Vector3(0,             p.y,   -p.z + .15f);
-  }
-
-  void colorChange(bool on) {
-    if (on) mat.SetColor("_SpecColor", offColor);
-    else mat.SetColor("_SpecColor", onColor);
-  }
-
-  public override void setState(manipState state) {
-    if (curState == state) return;
-
-    curState = state;
-
-    if (curState == manipState.none) {
-      colorChange(false);
+        if (lines[0] != null) lineWidth = lines[0].localScale.x;
     }
-    if (curState == manipState.selected) {
-      colorChange(true);
+
+    Vector3 controllerPosAtBeginDragging;
+    Transform tip;
+    public override void setGrab(bool on, Transform t)
+    {
+        base.setGrab(on, t);
+        tip = t.Find("manipCollViz").transform;
+        controllerPosAtBeginDragging = tip.position;
+        pAtBeginDragging = p;
     }
-    if (curState == manipState.grabbed) {
-      colorChange(true);
+
+    Vector3 p, pAtBeginDragging = Vector3.zero;
+    float lineWidth = 0f;
+
+    public override void grabUpdate(Transform t)
+    {
+        p = pAtBeginDragging + transform.InverseTransformPoint(tip.position) - transform.InverseTransformPoint(controllerPosAtBeginDragging);
+        p.x = Mathf.Clamp(p.x, -0.5f, 0.5f);
+        p.y = Mathf.Clamp(p.y, -0.5f, 0.5f);
+        p.z = Mathf.Clamp(p.z, -0.5f, 0.5f);
+        updatePercent(p, true);
+
+        if (manipulatorObjScript != null) manipulatorObjScript.hapticPulse((ushort)(700f * (_deviceInterface.percent.x + _deviceInterface.percent.y + _deviceInterface.percent.z) / 3 + 50f));
     }
-  }
+
+    void updatePercent(Vector3 tmp, bool invokeChange = false)
+    {
+        tmp.x = Mathf.Clamp01(-tmp.x + .5f);
+        tmp.y = Mathf.Clamp01(tmp.y + .5f);
+        tmp.z = Mathf.Clamp01(-tmp.z + .5f);
+
+        _deviceInterface.updatePercent(tmp, invokeChange);
+        updateLines(tmp);
+    }
+
+    float lineMargin = 0.0015f;
+    public void updateLines(Vector3 p)
+    {
+        p = p * (.3f - lineMargin * 2) + Vector3.one * lineMargin; // margin to align with inside borders
+        lines[0].localPosition = new Vector3(-p.x + .15f, p.y, 0);
+        lines[1].localPosition = new Vector3(-p.x + .15f, .15f, -p.z + .15f);
+        lines[2].localPosition = new Vector3(0, p.y, -p.z + .15f);
+    }
+
+    void colorChange(bool on)
+    {
+        if (on) mat.SetColor("_SpecColor", offColor);
+        else mat.SetColor("_SpecColor", onColor);
+    }
+
+    public override void setState(manipState state)
+    {
+        if (curState == state) return;
+
+        curState = state;
+
+        if (curState == manipState.none)
+        {
+            colorChange(false);
+        }
+        if (curState == manipState.selected)
+        {
+            colorChange(true);
+        }
+        if (curState == manipState.grabbed)
+        {
+            colorChange(true);
+        }
+    }
 }
