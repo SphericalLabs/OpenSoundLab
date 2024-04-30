faa5281833d2bd313bbcf3f7507d98a921774a55
Chregu <christoph.mueller90@protonmail.com>
2024-04-30 15:24:13 +0200

    Tested some things

--- Assets/Scripts/Networking/UniVoice/NetworkAudioManager.cs
+++ Assets/Scripts/Networking/UniVoice/NetworkAudioManager.cs
@@ -52,7 +52,8 @@ public class NetworkAudioManager : MonoBehaviour
     {
         agent = new ChatroomAgent(
             new UniVoiceMirrorNetwork(),
-            new UniVoiceUniMicInput(0, 8000, 25),
+            new UniVoiceBusAudioInput(0, 48000, 10),
+            //new UniVoiceUniMicInput(0, 8000, 25),
             new UniVoiceAudioSourceOutput.Factory()
         );
         agent.Network.OnCreatedChatroom += () => {
