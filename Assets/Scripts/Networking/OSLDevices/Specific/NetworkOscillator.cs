9a1ffa5cc39f0ba12a9b3cc9ae981d16ef356840
Chregu <christoph.mueller90@protonmail.com>
2024-08-06 15:36:34 +0200

    LFO change detection, sync on lfo

---
+++ Assets/Scripts/Networking/OSLDevices/Specific/NetworkOscillator.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class NetworkOscillator : NetworkSyncListener
+{
+    protected signalGenerator signalGenerator;
+    protected oscillatorDeviceInterface oscillatorDeviceInterface;
+    protected virtual void Awake()
+    {
+        signalGenerator = GetComponent<signalGenerator>();
+        oscillatorDeviceInterface = GetComponent<oscillatorDeviceInterface>();
+        oscillatorDeviceInterface.LfoChange += OnLfoChange;
+    }
+    #region Mirror
+    private void OnLfoChange(bool lfo)
+    {
+        if (lfo)
+        {
+            NetworkSyncEventManager.Instance.UpdateSync();
+        }
+    }
+
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        if (!isServer)
+        {
+            CmdRequestSync();
+        }
+    }
+
+    protected override void OnSync()
+    {
+        base.OnSync();
+        if (isServer)
+        {
+            RpcUpdatePhase(signalGenerator._phase);
+        }
+        else
+        {
+            CmdRequestSync();
+        }
+    }
+
+    protected override void OnIntervalSync()
+    {
+        base.OnIntervalSync();
+        if (isServer)
+        {
+            RpcUpdatePhase(signalGenerator._phase);
+        }
+    }
+
+    [Command(requiresAuthority = false)]
+    protected virtual void CmdRequestSync()
+    {
+        Debug.Log($"{gameObject.name} CmdRequestSync");
+        RpcUpdatePhase(signalGenerator._phase);
+    }
+
+    [ClientRpc]
+    protected virtual void RpcUpdatePhase(double phase)
+    {
+        if (isClient)
+        {
+            Debug.Log($"{gameObject.name} old phase: {signalGenerator._phase}, new phase {phase}");
+            signalGenerator._phase = phase;
+        }
+    }
+    #endregion
+}
